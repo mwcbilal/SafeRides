d5cc2bc6ebe24605df29f14c19ceaeadb5086bf5
Bilal Khan <bilal.khan@macworldtechnology.com>
2024-05-21 21:22:11 +0800

    formatting

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {MyStack} from './src/Navigators/StackNavigators';
+import { useColorScheme } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { MyStack } from './src/Navigators/StackNavigators';
 import { NavigationContainer } from '@react-navigation/native';
 
 function App(): JSX.Element {
@@ -13,11 +13,11 @@ function App(): JSX.Element {
 
   return (
     <NavigationContainer>
-<MyStack />
+      <MyStack />
     </NavigationContainer>
 
-  ) 
-  ;
+  )
+    ;
 }
 
 export default App;
